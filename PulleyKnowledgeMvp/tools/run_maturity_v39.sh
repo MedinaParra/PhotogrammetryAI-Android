e2a823426a05313456f63c1ba9264797a2a2678f
MedinaParra <33606993+MedinaParra@users.noreply.github.com>
2026-07-19 22:44:03 -0400

    Include coverage dependency in maturity runner

--- PulleyKnowledgeMvp/tools/run_maturity_v39.sh
+++ PulleyKnowledgeMvp/tools/run_maturity_v39.sh
@@ -4,5 +4,5 @@ R=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
 O="$R/build/maturity-v39"
 rm -rf "$O"
 mkdir -p "$O"
-javac -d "$O" "$R/app/src/main/java/cl/skm/pulleyai/CaptureReadiness.java" "$R/app/src/main/java/cl/skm/pulleyai/ReconstructionFrameSelectorCore.java" "$R/app/src/main/java/cl/skm/pulleyai/TrackPointRefinementCore.java" "$R/app/src/main/java/cl/skm/pulleyai/IndustrialAnalysisBudgetCore.java" "$R/tools/ReconstructionMaturityV38V39Test.java"
+javac -d "$O" "$R/app/src/main/java/cl/skm/pulleyai/CoveragePlanner.java" "$R/app/src/main/java/cl/skm/pulleyai/CaptureReadiness.java" "$R/app/src/main/java/cl/skm/pulleyai/ReconstructionFrameSelectorCore.java" "$R/app/src/main/java/cl/skm/pulleyai/TrackPointRefinementCore.java" "$R/app/src/main/java/cl/skm/pulleyai/IndustrialAnalysisBudgetCore.java" "$R/tools/ReconstructionMaturityV38V39Test.java"
 java -cp "$O" ReconstructionMaturityV38V39Test
